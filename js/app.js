ceaf1673fc0b8be7c4a51e6040515df3d23fd918
vanduc2514 <vanduc2514@gmail.com>
2020-02-22 14:10:38 +0700

    Thêm tính năng hiển thị số lần mouseover

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-// alert("There's only one thing");
-// alert("two do");
-// alert("there words");
-// alert("four you ...");
+alert("There's only one thing");
+alert("two do");
+alert("there words");
+alert("four you ...");
+let clickTime = 0;
 
 function moveRandom() {
     let horizontal = Math.floor(Math.random() * window.innerWidth);
     let vertical = Math.floor(Math.random() * window.innerHeight);
     document.getElementById("buttonNo").style.left = horizontal + 'px';
     document.getElementById("buttonNo").style.top = vertical + 'px';
+    clickTime = clickTime + 1;
+    if (clickTime < 10) {
+        document.getElementById("clickResult").innerHTML = "<b>Số lần click trượt: </b>" + clickTime;
+    } else {
+        alert("Đừng từ chối mình. Mình đáng yêu mà...");
+        document.getElementById("clickResult").innerHTML = "<b>Số lần từ chối: </b>" + clickTime;
+    }
+
 }
 
 function reset() {
