dbb90c208ac1cad3e7552332d54cb6b4bf4a5e75
vanduc2514 <vanduc2514@gmail.com>
2020-02-22 14:59:10 +0700

    Refractor lại code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 alert("There's only one thing");
-alert("two do");
+alert("two say");
 alert("there words");
 alert("four you ...");
-let clickTime = 0;
+let clickTimes = 0;
 
 function moveRandom() {
     let horizontal = Math.floor(Math.random() * window.innerWidth);
     let vertical = Math.floor(Math.random() * window.innerHeight);
     document.getElementById("buttonNo").style.left = horizontal + 'px';
     document.getElementById("buttonNo").style.top = vertical + 'px';
-    clickTime = clickTime + 1;
-    if (clickTime < 10) {
-        document.getElementById("clickResult").innerHTML = "<b>Số lần click trượt: </b>" + clickTime;
+    ++clickTimes;
+    if (clickTimes < 10) {
+        document.getElementById("clickResult").innerHTML = "<b>Số lần click trượt: </b>" + clickTimes;
     } else {
         alert("Đừng từ chối mình. Mình đáng yêu mà...");
-        document.getElementById("clickResult").innerHTML = "<b>Số lần từ chối: </b>" + clickTime;
+        document.getElementById("clickResult").innerHTML = "<b>Số lần từ chối: </b>" + clickTimes;
     }
 
 }
