f5b2f2415d3940c6afafdef7c209e8a33b9831e1
vanduc2514 <vanduc2514@gmail.com>
2020-02-22 13:31:17 +0700

    Sửa lại layout

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function moveRandom() {
+    let horizontal = Math.floor(Math.random() * window.innerWidth);
+    let vertical = Math.floor(Math.random() * window.innerHeight);
+    document.getElementById("buttonNo").style.left = horizontal + 'px';
+    document.getElementById("buttonNo").style.top = vertical + 'px';
+}
+
+function reset() {
+    document.getElementById("buttonNo").style.top = "480px";
+    document.getElementById("buttonNo").style.left = "1000px";
+}
